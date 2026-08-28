a0e291d9c349de1e64e407f2ba4ae85632ca9b7c
chaslinux <chaslinux@gmail.com>
2026-08-28 13:20:56 -0400

    Checkpoint 011: improve dungeon layout

--- src/dungeon.c
+++ src/dungeon.c
@@ -23,6 +23,7 @@ void journey_dungeon_init(
         {
             dungeon->tiles[x][y].type =
                 JOURNEY_DUNGEON_FLOOR;
+
             dungeon->tiles[x][y].walkable = true;
         }
     }
@@ -34,10 +35,12 @@ void journey_dungeon_init(
     {
         dungeon->tiles[x][0].type =
             JOURNEY_DUNGEON_WALL;
+
         dungeon->tiles[x][0].walkable = false;
 
         dungeon->tiles[x][JOURNEY_DUNGEON_HEIGHT - 1].type =
             JOURNEY_DUNGEON_WALL;
+
         dungeon->tiles[x][JOURNEY_DUNGEON_HEIGHT - 1].walkable =
             false;
     }
@@ -46,18 +49,23 @@ void journey_dungeon_init(
     {
         dungeon->tiles[0][y].type =
             JOURNEY_DUNGEON_WALL;
+
         dungeon->tiles[0][y].walkable = false;
 
         dungeon->tiles[JOURNEY_DUNGEON_WIDTH - 1][y].type =
             JOURNEY_DUNGEON_WALL;
+
         dungeon->tiles[JOURNEY_DUNGEON_WIDTH - 1][y].walkable =
             false;
     }
 
     /*
-     * Central wall dividing the dungeon.
+     * Central wall.
+     *
+     * This divides the dungeon into western and
+     * eastern sections, with a passage near the middle.
      */
-    for (int y = 3; y <= 20; ++y)
+    for (int y = 2; y <= 20; ++y)
     {
         if (y == 11 || y == 12)
         {
@@ -66,7 +74,104 @@ void journey_dungeon_init(
 
         dungeon->tiles[16][y].type =
             JOURNEY_DUNGEON_WALL;
+
         dungeon->tiles[16][y].walkable = false;
+    }
+
+    /*
+     * Western room.
+     *
+     * Add a smaller enclosed room in the northwest.
+     */
+    for (int x = 3; x <= 11; ++x)
+    {
+        dungeon->tiles[x][5].type =
+            JOURNEY_DUNGEON_WALL;
+
+        dungeon->tiles[x][5].walkable = false;
+
+        dungeon->tiles[x][10].type =
+            JOURNEY_DUNGEON_WALL;
+
+        dungeon->tiles[x][10].walkable = false;
+    }
+
+    for (int y = 5; y <= 10; ++y)
+    {
+        dungeon->tiles[3][y].type =
+            JOURNEY_DUNGEON_WALL;
+
+        dungeon->tiles[3][y].walkable = false;
+
+        dungeon->tiles[11][y].type =
+            JOURNEY_DUNGEON_WALL;
+
+        dungeon->tiles[11][y].walkable = false;
+    }
+
+    /*
+     * Doorway into the western room.
+     */
+    dungeon->tiles[7][10].type =
+        JOURNEY_DUNGEON_FLOOR;
+
+    dungeon->tiles[7][10].walkable = true;
+
+    /*
+     * Eastern room.
+     *
+     * Add a larger chamber in the northeast.
+     */
+    for (int x = 20; x <= 28; ++x)
+    {
+        dungeon->tiles[x][4].type =
+            JOURNEY_DUNGEON_WALL;
+
+        dungeon->tiles[x][4].walkable = false;
+
+        dungeon->tiles[x][10].type =
+            JOURNEY_DUNGEON_WALL;
+
+        dungeon->tiles[x][10].walkable = false;
+    }
+
+    for (int y = 4; y <= 10; ++y)
+    {
+        dungeon->tiles[20][y].type =
+            JOURNEY_DUNGEON_WALL;
+
+        dungeon->tiles[20][y].walkable = false;
+
+        dungeon->tiles[28][y].type =
+            JOURNEY_DUNGEON_WALL;
+
+        dungeon->tiles[28][y].walkable = false;
+    }
+
+    /*
+     * Doorway into the eastern room.
+     */
+    dungeon->tiles[20][7].type =
+        JOURNEY_DUNGEON_FLOOR;
+
+    dungeon->tiles[20][7].walkable = true;
+
+    /*
+     * Southern interior wall.
+     *
+     * This creates a smaller southern chamber.
+     */
+    for (int x = 4; x <= 27; ++x)
+    {
+        if (x == 8 || x == 24)
+        {
+            continue;
+        }
+
+        dungeon->tiles[x][17].type =
+            JOURNEY_DUNGEON_WALL;
+
+        dungeon->tiles[x][17].walkable = false;
     }
 
     /*
@@ -74,6 +179,7 @@ void journey_dungeon_init(
      */
     dungeon->tiles[16][22].type =
         JOURNEY_DUNGEON_EXIT;
+
     dungeon->tiles[16][22].walkable = true;
 }
 
