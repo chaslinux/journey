8f18e59ce5c16149dfa185e9eb5d2f1671c50baf
chaslinux <chaslinux@gmail.com>
2026-08-31 13:37:05 -0400

    Remove duplicate monster declaration

--- src/monster.h
+++ src/monster.h
@@ -54,13 +54,6 @@ void journey_monster_init(
     int y
 );
 
-void journey_monster_init(
-    JourneyMonster *monster,
-    const JourneyMonsterDefinition *definition,
-    int x,
-    int y
-);
-
 void journey_monster_take_damage(
     JourneyMonster *monster,
     int damage
